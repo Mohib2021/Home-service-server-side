660d7cbb5fe73d1a6defa1038f8978d0096f451b
Mohib2021 <mdmohibullasheikh@gmail.com>
2021-12-17 01:34:07 +0600

    get single service

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 const uri =
 	"mongodb+srv://Mohib:Mohib@cluster0.nr9ns.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -25,6 +32,15 @@ const run = async () => {
 			const result = await cursor.toArray();
 			res.send(result);
 		});
+
+		//Get single service
+		app.get("/services/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const result = await serviceCollection.findOne(query);
+			res.send(result);
+		});
+
 		//Get User Collections
 		app.get("/users", async (req, res) => {
 			const cursor = userCollections.find({});
