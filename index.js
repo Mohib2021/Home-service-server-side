aca0f51aafa602a97b255359762681ca7a3dbe08
Mohib2021 <mdmohibullasheikh@gmail.com>
2021-12-19 02:16:15 +0600

    remove middleware

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
-app.use(express.json());
 app.use(fileUpload());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 const uri =
 	"mongodb+srv://Mohib:Mohib@cluster0.nr9ns.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
